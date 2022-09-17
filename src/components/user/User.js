adbd2ee96e57c591eb022c2c359a0ed6034c31ea
Rafath-Bin-Zafar-Auvee <rbza98@outlook.com>
2022-09-17 17:51:19 +0600

    Complete

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,40 +1,109 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-const User = () => {
-  axios
-    .get("https://api.github.com/users/rafath-bin-zafar-auvee")
-    .then(function (res) {
-      // handle success
-      setAvater(res.data.avatar_url)
-      console.log(res);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
+import { MdLocationOn } from "react-icons/md";
+import Pagination from "./../pagination/pagination";
 
-  const [avater, setAvater] = useState();
+const User = () => {
+  const [avater, setAvater] = useState("");
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/Rafath-Bin-Zafar-Auvee/repos")
+      .then(function (res) {
+        setRepos(res.data);
+        console.log(res.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        console.log("Complete");
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/rafath-bin-zafar-auvee")
+      .then(function (res) {
+        // handle success
+        setAvater(res.data);
+        // console.log(res.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // console.log("Complete");
+      });
+  }, []);
+  
+
+
+  // Pagination Code
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = repos.slice(firstPostIndex, lastPostIndex);
 
   return (
-    <div className="hero min-h-screen ">
-      <div className="hero-content flex-col lg:flex-row">
-        <img
-          src={avater}
-          className="max-h-[350px] max-w-[350px] rounded-lg shadow-2xl rounded-full"
-          alt=""
-        />
-        <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+    <div>
+      <div className="hero min-h-[350px] ">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src={avater.avatar_url}
+            className="max-h-[250px] max-w-[250px] border-4 border-sky-500 rounded-lg shadow-2xl rounded-full"
+            alt=""
+          />
+          <div>
+            <h1 className="text-3xl font-bold">{avater.name}</h1>
+
+            <p className="pt-6  text-sm">{avater.bio}</p>
+            <div className="flex flex-row mt-[5px]">
+              <p className="mr-[5px] font-bold text-2xl">
+                <MdLocationOn />
+              </p>
+              <p className=" text-sm">{avater.location}</p>
+            </div>
+          </div>
         </div>
+      </div>
+      {/* <h1>Length={repos.length}</h1>/ */}
+      <div className="grid justify-items-center grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-[50px]">
+      {currentPosts.map((repo, index, arr) => (
+        <div
+          key={index}
+          className="card w-[550px] h-[200px] rounded-none  border-4 border-black"
+        >
+          <div className="card-body">
+            <h2 className="card-title text-2xl text-sky-500">
+              {repo.name}
+            </h2>
+            <h3>{repo.description}</h3>
+            <div className="card-actions ">
+            <button
+                
+                className="btn btn-outline bg-sky-500 text-white btn-xs"
+              >
+                Pinned
+              </button>
+            </div>
+          </div>
+        </div>
+      ))}  
+      </div>
+      <div className="grid justify-items-center mt-[100px]">
+        <Pagination
+          totalPosts={repos.length}
+          postsPerPage={postsPerPage}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+          
+        />
       </div>
     </div>
   );
